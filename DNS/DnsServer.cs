2ba9ce25805fda18dab2406de1054565e6344a37
dickrd <dickdata7@gmail.com>
2014-10-12 10:56:55 +0800

    Include necessary file.

---
+++ DNS/DnsServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNSwitchy
+{
+    public class DnsServer
+    {
+        public string Name { get; set; }
+        public string PrimaryAddress { get; set; }
+        public string SecondaryAddress { get; set; }
+    }
+}
